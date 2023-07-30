b7c7cbe23f0ccab42cf25f85a055860e1750da13
Natalja <tasia@inbox.lt>
2023-07-30 14:42:41 +0300

    initial commit

---
+++ BankasU2/index.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/bootstrap.php';
+$title = 'Bankas_2';
+require __DIR__ . '/top.php';
+?>
+<?php require __DIR__ . '/menu.php' ?>
+<div class = 'home'>
+    <h1>Bankas</h1>
+</div>
+
+<?php
+require __DIR__ . '/bottom.php';
---
+++ BankasU2/menu.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="<?= URL ?>index.php">Home</a></li>
+        <li><a href="<?= URL ?>list.php">Colors</a></li>
+        <li><a href="<?= URL ?>create.php">Add Color</a></li>
+    </ul>
+</nav>
